a9d8d60be5a88fa5cc3e7e9db058f006faa6a110
eyssette <cedric.eyssette@gmail.com>
2022-03-13 05:29:35 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			define: {
+			  'process.env': process.env,
+			},
+		  }
 	}
 };
 
