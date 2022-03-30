c5177b3e0c82f8d3e00f7c39e401d9efd5aa04ba
eyssette <cedric.eyssette@gmail.com>
2022-03-30 19:25:23 +0200

    Update configEnvironment.js

--- src/routes/configEnvironment.js
+++ src/routes/configEnvironment.js
@@ -1,2 +1,6 @@
-// export const cryptedModeKey = import.meta.env.VITE_CRYPTED_MODE_KEY;
-export const cryptedModeKey = process.env.VITE_CRYPTED_MODE_KEY;
+export let cryptedModeKey = ''
+if (process.env.NODE_ENV == 'development') {
+	cryptedModeKey = import.meta.env.VITE_CRYPTED_MODE_KEY;
+} else {
+	cryptedModeKey = process.env.VITE_CRYPTED_MODE_KEY;
+}
